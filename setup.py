d6f41a38cff6f4eafbc27df8ad7a8ff98c99f7d8
Jared McKnight <jmcknight@archertech.ca>
2021-03-16 09:36:29 -0400

    bump version to 0.1.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 name = 'wireguard'
 description = 'Wireguard Utilities'
-version = '0.1.5'
+version = '0.1.6'
 author = 'Fictive Kin LLC'
 email = 'hello@fictivekin.com'
 classifiers = [
