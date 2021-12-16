7a4bf431162a13f5c61e42b3f87fb631de98de25
Markus Neubauer <neubauer@email-online.org>
2021-12-16 18:43:47 +0100

    improved table value check

--- wireguard/peer.py
+++ wireguard/peer.py
@@ -330,11 +330,11 @@ class Peer:  # pylint: disable=too-many-instance-attributes
 
             if isinstance(value, int) and not \
                     (0 < value < 253 or 255 < value < (2**31)):  # pylint: disable=no-else-raise
-                raise ValueError('Table must be in the ranges 1-252, 256-(2°31-1)')
+                raise ValueError('Table must be in the ranges 1-252, 256-(2^31-1)')
 
             # special values allowed (auto=default, off=no route created)
             # ref: https://git.zx2c4.com/wireguard-tools/about/src/man/wg-quick.8
-            elif value not in ('auto', 'off'):
+            elif not isinstance(value, str) or value not in ('auto', 'off'):
                 raise ValueError('Table must be "auto", "off" or an integer value')
 
         self._table = value
